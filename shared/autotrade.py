28038b58d88757fd8113938e1a9787f803e59fc1
Carlos Wu Fei <carkod@Carloss-MacBook-Pro.local>
2024-04-20 03:22:38 +0200

    Use new simplied balance estimation endpoints to fix timeout

--- shared/autotrade.py
+++ shared/autotrade.py
@@ -3,12 +3,10 @@ import math
 import logging
 
 from datetime import datetime
-from api.tools.enum_definitions import KafkaTopics
 from producers.base import BaseProducer
-from shared.enums import CloseConditions
+from shared.enums import CloseConditions, KafkaTopics, Strategy
 from models.signals import BotPayload, SignalsConsumer, TrendEnum
 from shared.exceptions import AutotradeError
-from shared.enums import Strategy
 from shared.apis import BinbotApi
 from shared.utils import round_numbers, supress_notation
 
@@ -275,12 +273,13 @@ class Autotrade(BaseProducer, BinbotApi):
 
         else:
             message = f"Succesful {self.db_collection_name} autotrade, opened with {self.pair}!"
-            value = {
-                "botId": botId,
-                "message": message
-            }
+            value = {"botId": botId, "message": message}
             self.submit_bot_event_logs(botId, message)
             # Send message to restart streaming at the end to avoid blocking
             # Message is sent only after activation is successful,
             # if bot activation failed, we want to try again with a new bot
-            self.producer.send(KafkaTopics.restart_streaming.value, value=json.dumps(value)).add_callback(self.base_producer.on_send_success).add_errback(self.base_producer.on_send_error)
+            self.producer.send(
+                KafkaTopics.restart_streaming.value, value=json.dumps(value)
+            ).add_callback(self.base_producer.on_send_success).add_errback(
+                self.base_producer.on_send_error
+            )
