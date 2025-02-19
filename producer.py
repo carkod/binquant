070afe5d208fcef12201f1b4e1e396ffe9e8a5c9
Carlos Wu Fei <carkodw@gmail.com>
2025-02-19 13:38:02 +0000

    Set default level of logs using env vars

--- producer.py
+++ producer.py
@@ -1,8 +1,15 @@
 import asyncio
 import logging
-
+import os
 from producers.base import BaseProducer
 from producers.klines_connector import KlinesConnector
+
+logging.basicConfig(
+    level=os.environ["LOG_LEVEL"],
+    filename=None,
+    format="%(asctime)s.%(msecs)03d UTC %(levelname)s %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 async def main():
