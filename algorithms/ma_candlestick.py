9fbda13c0b2e2236e50a594efbc04765352063e1
Carlos Wu Fei <carkodw@gmail.com>
2025-06-17 00:33:06 +0200

    Performance improvements

--- algorithms/ma_candlestick.py
+++ algorithms/ma_candlestick.py
@@ -3,7 +3,7 @@ import os
 from typing import TYPE_CHECKING
 
 from models.signals import BollinguerSpread, SignalsConsumer
-from shared.enums import KafkaTopics, MarketDominance, Strategy
+from shared.enums import KafkaTopics, Strategy, MarketDominance
 from shared.utils import round_numbers
 
 if TYPE_CHECKING:
@@ -117,7 +117,9 @@ async def ma_candlestick_jump(
         bot_strategy = cls.bot_strategy
         btc_correlation = cls.binbot_api.get_btc_correlation(symbol=cls.symbol)
 
-        if cls.current_market_dominance == MarketDominance.GAINERS:
+        if (
+            cls.current_market_dominance == MarketDominance.GAINERS
+        ):
             # market is bullish, most prices increasing,
             # but looks like it's dropping and going bearish (reversal)
             # candlesticks of this specific crypto are seeing a huge jump (candlstick jump algo)
