3ab4a0c3556877f07c95326dd37c4c5877adb565
Carlos Wu Fei <carkodw@gmail.com>
2025-07-27 14:51:18 +0100

    Test new semaphore data processing

--- consumer.py
+++ consumer.py
@@ -10,6 +10,10 @@ from consumers.klines_provider import KlinesProvider
 from consumers.telegram_consumer import TelegramConsumer
 from shared.enums import KafkaTopics
 from shared.rebalance_listener import RebalanceListener
+from collections import defaultdict
+from asyncio import create_task, gather, Semaphore
+from aiokafka.structs import TopicPartition
+
 
 logging.basicConfig(
     level=os.environ["LOG_LEVEL"],
@@ -28,28 +32,55 @@ async def data_process_pipe() -> None:
         enable_auto_commit=False,
         session_timeout_ms=30000,
         heartbeat_interval_ms=10_000,
-        max_poll_records=1,
+        max_poll_records=5,
+        max_poll_interval_ms=600_000,  # added to handle long processing
     )
 
-    # Set rebalance listener
     rebalance_listener = RebalanceListener(consumer)
     consumer.subscribe(
         [KafkaTopics.klines_store_topic.value], listener=rebalance_listener
     )
 
-    # Start dependencies before the consumer to avoid timeouts
     klines_provider = KlinesProvider(consumer)
     await klines_provider.load_data_on_start()
+
+    sem = Semaphore(10)
+    last_offsets = defaultdict(int)
+    pending_tasks = []
+
+    async def handle_message(message):
+        tp = (message.topic, message.partition)
+        async with sem:
+            await klines_provider.aggregate_data(message.value)
+            last_offsets[tp] = message.offset
 
     try:
         await consumer.start()
         async for message in consumer:
-            if message.topic == KafkaTopics.klines_store_topic.value:
-                await klines_provider.aggregate_data(message.value)
+            # Dispatch async task
+            task = create_task(handle_message(message))
+            pending_tasks.append(task)
 
-            await consumer.commit()
+            if len(pending_tasks) >= 20:  # tune this threshold
+                await gather(*pending_tasks)
+                pending_tasks.clear()
+
+                offsets = {
+                    TopicPartition(topic, partition): offset + 1
+                    for (topic, partition), offset in last_offsets.items()
+                }
+                await consumer.commit(offsets=offsets)
 
     finally:
+        await gather(*pending_tasks)
+        # Final commit
+        if last_offsets:
+            offsets = {
+                TopicPartition(topic, partition): offset + 1
+                for (topic, partition), offset in last_offsets.items()
+            }
+            await consumer.commit(offsets=offsets)
+
         await consumer.stop()
 
 
