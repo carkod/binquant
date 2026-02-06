9139e4a3cc4de92adff679d9da965ddb79820fe3
Carlos Wu <carlos.wufei@houseful.co.uk>
2026-02-06 18:48:58 +0000

    Replace max_window ms with internval countdown

--- consumers/signal_collector.py
+++ consumers/signal_collector.py
@@ -1,16 +1,22 @@
-from time import time
-from models.signals import SignalCandidate
 from collections.abc import Callable
-from pybinbot import BinbotApi
+from time import time
+
+from pybinbot import BinbotApi, KucoinKlineIntervals
+
+from models.signals import SignalCandidate
 
 
 class SignalCollector:
-    def __init__(self, max_window_ms=200):
+    def __init__(self):
         self.buffer: dict[str, SignalCandidate] = {}
-        self.max_window_ms = max_window_ms
         self.first_seen_at: int | None = None
         self.autotrade = None
         self.binbot_api = BinbotApi()
+        self.autotrade_settings = self.binbot_api.get_autotrade_settings()
+        if self.autotrade_settings is not None:
+            self.interval = self.autotrade_settings["candlestick_interval"]
+        else:
+            self.interval = KucoinKlineIntervals.FIFTEEN_MINUTES
 
     def rank(self) -> list[SignalCandidate]:
         return sorted(
@@ -32,7 +38,9 @@ class SignalCollector:
 
         self.buffer[candidate.symbol] = candidate
 
-        if now - self.first_seen_at >= self.max_window_ms:
+        max_window_ms = KucoinKlineIntervals.get_interval_ms(self.interval) * 2
+
+        if now - self.first_seen_at >= max_window_ms:
             await self.flush()
 
     async def flush(self):
@@ -46,5 +54,7 @@ class SignalCollector:
         active_bots = self.binbot_api.get_active_pairs()
 
         for candidate in ranked:
+            if self.autotrade and candidate.symbol in active_bots:
+                await self.autotrade(candidate)
             if self.autotrade and candidate.symbol in active_bots:
                 await self.autotrade(candidate)
